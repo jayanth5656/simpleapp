90d641288213713e25c7e91093920d4985a53c59
jayanth5656 <133353573+jayanth5656@users.noreply.github.com>
2023-09-12 10:56:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code for the web page
+document.getElementById('clickMeButton').addEventListener('click', function() {
+    const outputText = document.getElementById('outputText');
+    outputText.textContent = 'Button Clicked!';
+});
